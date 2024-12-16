c7fcf69fd57fe1bcfd1d89a75e188aef43975293
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-12-16 21:18:18 +0000

    Updated comment about priority.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,8 @@ return {
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
+    -- Priority ensures it loads the colour scheme first
+    -- before anything else
     priority = 1000,
     opts = {
       colorscheme = "dracula",
