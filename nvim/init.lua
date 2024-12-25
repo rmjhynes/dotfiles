3a4f98cc5e91ed87e31dd9bbd4b0c0554294741d
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-12-25 21:26:38 +0000

    Removed setting to keep cursor 10 lines above or below top or bottom of file.

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,9 +74,6 @@ vim.opt.inccommand = "split"
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
--- Minimal number of screen lines to keep above and below the cursor.
--- vim.opt.scrolloff = 10
-
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
