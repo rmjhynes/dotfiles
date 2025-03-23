e4a0751c85b1fbc6c392136e34775b7c563467ab
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-03-23 14:46:42 +0000

    Updated dashboard button to create a new file.

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -23,7 +23,7 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
-        dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
         dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
         dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
         dashboard.button("s", "  Settings", ":e $MYVIMRC<CR>"),
