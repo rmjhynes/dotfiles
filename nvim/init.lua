83b975c483de863853bd0f09cbafec8f2140aec7
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-03-23 15:54:57 +0000

    Removed highlight yank config as this is now configured with tiny-glimmer.

--- nvim/init.lua
+++ nvim/init.lua
@@ -118,17 +118,6 @@ vim.keymap.set('n', '<leader>/', ':vsplit<CR>', { desc = 'Vertical Split' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
---vim.api.nvim_create_autocmd("TextYankPost", {
---	desc = "Highlight when yanking (copying) text",
---	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
---	callback = function()
---		vim.highlight.on_yank()
---	end,
---})
-
 -- Needed for buffeline.nvim plugin to work
 vim.opt.termguicolors = true
 
