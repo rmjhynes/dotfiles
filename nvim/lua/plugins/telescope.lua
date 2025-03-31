f212707e3c221d55800ffebda8d0e99cc7244c7e
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-03-31 20:55:22 +0100

    Updated telescope to include hidden files in its search.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
       telescope.setup({})
 
       -- Keymaps for Telescope commands
-      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true no_ignore=true<CR>", { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { noremap = true, silent = true })
