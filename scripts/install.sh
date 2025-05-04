596a4d876ce35df2485708d24c608dd936f87d72
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-05-04 21:30:08 +0100

    Revert "feat: update install script to install nix pakcages"
    This reverts commit 82726e63a469c54d557db454b46f9f1ef1877aad.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,9 +2,6 @@
 
 # If running a Linux devpod, install packages using nixpkgs
 if [[ $(uname) == "Linux"]]; then
-  # Symlink config.nix file
-  ln -sfv ~/.config/nixpkgs/config.nix ~/config.nix
-  # Install packages declared in config.nix
   nix-env -iA nixpkgs.myPackages
 fi
 
