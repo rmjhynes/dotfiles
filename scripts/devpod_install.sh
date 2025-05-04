9e01da7a4502e8e06bdbab314c937fd5047f0fb6
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-05-04 21:43:26 +0100

    fix: create directories where nix packages file will live

--- scripts/devpod_install.sh
+++ scripts/devpod_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Create directories where nix packages file will live
+mkdir ~/.config && mkdir ~/.config/nixpkgs
+
 # Symlink config.nix file
 ln -sfv ~/.config/nixpkgs/config.nix ~/config.nix
+
 # Install packages declared in config.nix
 nix-env -iA nixpkgs.myPackages
