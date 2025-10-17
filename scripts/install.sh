6bdcc3502027caf23932f70996b0a7d1468307c3
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-10-17 19:52:11 +0100

    refactor: add username to git clone commands for linux compatibility

--- scripts/install.sh
+++ scripts/install.sh
@@ -54,32 +54,32 @@ fi
 ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 if [[ ! -d $ZSH_CUSTOM/themes/powerlevel10k ]]; then
   echo "Installing Powerlevel10k theme..."
-  git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+  git clone https://rmjhynes@github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 fi
 
 # Install zsh-autosuggestions plugin if not already installed
 if [[ ! -d $ZSH_CUSTOM/plugins/zsh-autosuggestions ]]; then
   echo "Installing zsh-autosuggestions..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+  git clone https://rmjhynes@github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
 
 # Install zsh-syntax-highlighting plugin if not already installed
 if [[ ! -d $ZSH_CUSTOM/plugins/zsh-syntax-highlighting ]]; then
   echo "Installing zsh-syntax-highlighting..."
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+  git clone https://rmjhynes@github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 fi
 
 # Configure Dracula theme for vim if not already installed
 echo "Setting up Dracula theme for vim..."
 mkdir -p ~/.vim/pack/themes/start
 if [[ ! -d ~/.vim/pack/themes/start/dracula ]]; then
-  git clone https://github.com/dracula/vim.git ~/.vim/pack/themes/start/dracula
+  git clone https://rmjhynes@github.com/dracula/vim.git ~/.vim/pack/themes/start/dracula
 fi
 
 # Install tmux package manager if not already installed
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
   echo "Installing TPM (tmux plugin manager)..."
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone https://rmjhynes@github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 # Source updates to TPM in tmux configuration file and reload
